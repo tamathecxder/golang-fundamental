c9ea18695ec7f6e8cde1e587b07969f853fcbed3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-10-16 09:29:01 +0700

    chore: moving all related go operations to separete folder

---
+++ operations/comparation_operation.go
@@ -0,0 +1,15 @@
+package datatypes
+
+import "fmt"
+
+func ComparationOperations() {
+	var name1 = "agus"
+	var name2 = "ujang"
+
+	fmt.Println(name1 == name2)
+	fmt.Println(name1 > name2)
+	fmt.Println(name1 < name2)
+	fmt.Println(name1 >= name2)
+	fmt.Println(name1 <= name2)
+	fmt.Println(name1 != name2)
+}
